14140d9b7f049409c06cb99b1fb1ab55d4348fe6
nihaprezz <sergio13prez@gmail.com>
2019-10-05 22:08:32 -0400

    done

--- app/application.rb
+++ app/application.rb
@@ -13,4 +13,29 @@ class Application
 
         resp.finish #the response isnt sent back to the user until finish is called. 
     end
+
+    # @@items = ["Apples", "Carrots", "Pears"]
+
+    # def call(env)
+    #     resp = Rack::Response.new
+    #     req = Rack::Request.new(env) 
+
+    #     if req.path.match(/items/)
+    #         @@items.each do |item|
+    #             resp.write "#{item}\n"
+    #         end
+    #     elsif req.path.match(/search/)
+    #         search_term = req.params['q']
+
+    #         if @@items.include?(search_term)
+    #             resp.write "#{search_term} is one of our items"
+    #         else
+    #             resp.write "Couldn't find #{search_term}"
+    #         end
+    #     else
+    #         resp.write "Path Not Found"
+    #     end
+
+    #     resp.finish
+    # end
 end
