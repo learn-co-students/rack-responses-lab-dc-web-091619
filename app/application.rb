0779d0ca1fbebca53a4301db3491db0bc665201c
Graham Flaspoehler <grahamflas@gmail.com>
2019-10-07 12:14:52 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -14,3 +14,35 @@ class Application
   end
 
 end
+
+# class Application 
+
+#   @@items = %w(Apples Carrots Bananas)
+
+#   def call(env)
+#     req = Rack::Request.new(env)
+#     resp = Rack::Response.new
+
+#     if req.path.match(/items/)
+#       @@items.each do |item|
+#         resp.write "#{item}\n"
+#       end
+
+#     elsif req.path.match(/search/)
+#       search_term = req.params["q"]
+
+#       if @@items.include?(search_term)
+#         resp.write ("#{search_term} is one of our items.")
+
+#       else
+#         resp.write ("Could not find #{search_term}")
+#       end
+
+#     else
+#       resp.write "Path Not Found"
+#     end
+    
+#     resp.finish
+#   end
+
+# end
